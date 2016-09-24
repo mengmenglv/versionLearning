3d24c19ec26748764e770cf9df7c68f8ffaf98aa
lvmengmeng <lvmengmeng@meituan.com>
2016-09-24 17:19:44 +0800

    dev修改c

--- versionLearning/c.js
+++ versionLearning/c.js
@@ -1,18 +1,3 @@
-/**
- * Created by  on 16/9/24.
- *
- * df;gk;ldkg;dg
- */
-
-//测试提交
-//dev 分支修改新增
-//fdgs
-//dev分支
-
-function a() {
-    console.log("lkjfl")
-}
-function devgter() {
-    console.log(",kdjglkgjd")
-console.log("master+devkjgkjg")
+function devtext() {
+    console.log("lkdjlkdlg")
 }
