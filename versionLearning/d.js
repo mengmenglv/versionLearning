9d8b28da7ba13b3facb80285016040171e51aa92
lvmengmeng <lvmengmeng@meituan.com>
2016-09-24 17:53:32 +0800

    dev-a

---
+++ versionLearning/d.js
@@ -0,0 +1,17 @@
+/**
+ * Created by lvmengmeng on 16/9/24.
+ */
+
+//测试提交
+var a = 2;
+var b = 3;
+var c = 5;
+function sum(a,b) {
+    //sum增加注释
+    return a*b;
+}
+
+var d = sum(a,b);
+alert(d);
+
+//在此修改dev测试
