5d3e0645bb8b3e5905191f436f9c0b4c74080fa0
lvmengmeng <lvmengmeng@meituan.com>
2016-09-24 18:20:36 +0800

    master

--- versionLearning/d.js
+++ versionLearning/d.js
@@ -2,16 +2,4 @@
  * Created by lvmengmeng on 16/9/24.
  */
 
-//测试提交
-var a = 2;
-var b = 3;
-var c = 5;
-function master(a,b,c) {
-    //sum增加注释
-    return a+b+c;
-}
-
-var d = sum(a,b);
-alert(d);
-
-//在此修改dev测试
+//删除d文件
