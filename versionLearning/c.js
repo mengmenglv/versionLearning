b2129ba93cb45a92e1e57a66dbb0b5bb5194a9d8
lvmengmeng <lvmengmeng@meituan.com>
2016-09-24 17:39:15 +0800

    master

--- versionLearning/c.js
+++ versionLearning/c.js
@@ -1,19 +1,7 @@
 
 
-//测试提交
-//dev 分支修改新增
-//fdgs
-//dev分支
-function dwef() {
 
-}
-function devMlkldfl() {
-    console.log("lkjfl")
-}
-function devgter() {
-    console.log(",kdjglkgjd")
-console.log("master+devkjgkjg")
-}
-function df() {
-    console.log("klldjfg")
+//dev分支
+function dwefDev() {
+
 }
